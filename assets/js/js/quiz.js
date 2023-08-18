56a9e5b16cfe014bef8c7cdeeb3e3e61c9e1b9a2
Anthony Murray <anthonymurray1@gmail.com>
2023-08-18 20:45:04 +0100

    Define questions for the quiz.

--- assets/js/js/quiz.js
+++ assets/js/js/quiz.js
@@ -6,3 +6,103 @@ startButton.addEventListener('click', () => {
   startButton.style.display = 'none';
   optionsContainer.style.display = 'block';
 });
+
+const quizQuestions = [
+  {
+    answer: 0,
+    options: [
+      "Tony Scott", "Ridley Scott", "Steven Spielberg", "James Cameron"
+    ],
+    question: "Who directed the original Top Gun film?"
+  },
+  {
+    answer: 2,
+    options: ["Iceman", "Ghost", "Maverick", "Viper"],
+    question: "What was Tom Cruise's character's call sign in Top Gun?"
+  },
+  {
+    answer: 0,
+    options: ["Kelly McGillis", "Meg Ryan", "Nicole Kidman", "Julia Roberts"],
+    question: "Who played the role of Charlie in Top Gun?"
+  },
+  {
+    answer: 3,
+    options: ["Sky High", "Fighter School", "Navy Pilot Academy", "TOPGUN"],
+    question: "What is the name of the flight school in Top Gun?"
+  },
+  {
+    answer: 2,
+    options: ["F-16 Falcon", "F-22 Raptor", "F-14 Tomcat", "F-15 Eagle"],
+    question: "What type of aircraft does Maverick primarily fly in Top Gun?"
+  },
+  {
+    answer: 1,
+    options: ["Val Kilmer", "Anthony Edwards", "Michael Ironside", "Tom Skerritt"],
+    question: "Who played the role of Goose in Top Gun?"
+  },
+  {
+    answer: 1,
+    options: ["Shot down", "Ejector seat accident", "Engine failure", "Explosion on the ground"],
+    question: "How does Goose die in Top Gun?"
+  },
+  {
+    answer: 1,
+    options: ["Eye of the Tiger", "Take My Breath Away", "Stairway to Heaven", "We Will Rock You"],
+    question: "What famous song is associated with Top Gun?"
+  },
+  {
+    answer: 1,
+    options: ["Tony Scott", "Joseph Kosinski", "James Cameron", "Christopher Nolan"],
+    question: "Who is directing Top Gun: Maverick?"
+  },
+  {
+    answer: 2,
+    options: ["10 years", "20 years", "30 years", "40 years"],
+    question: "How many years after the original does Top Gun: Maverick take place?"
+  },
+  {
+    answer: 0,
+    options: ["Goose's son", "A new instructor", "An antagonist pilot", "Maverick's son"],
+    question: "What role does Miles Teller play in Top Gun: Maverick?"
+  },
+  {
+    answer: 0,
+    options: ["Hans Zimmer", "John Williams", "Alan Silvestri", "Danny Elfman"],
+    question: "Who composed the score for Top Gun: Maverick?"
+  },
+  {
+    answer: 0,
+    options: ["Yes", "No"],
+    question: "Is the F-14 Tomcat featured in Top Gun: Maverick?"
+  },
+  {
+    answer: 1,
+    options: ["Maverick only", "Maverick and Iceman", "Maverick, Iceman, and Viper", "Maverick and Charlie"],
+    question: "Which original characters return in Top Gun: Maverick?"
+  },
+  {
+    answer: 3,
+    options: ["F-35 Lightning II", "F-22 Raptor", "F-18 Super Hornet", "All of the above"],
+    question: "What new aircraft are introduced in Top Gun: Maverick?"
+  },
+  {
+    answer: 1,
+    options: ["Tom Hanks", "Tom Cruise", "Brad Pitt", "Johnny Depp"],
+    question: "Who plays the character Maverick in Top Gun?"
+  },
+  {
+    answer: 0,
+    options: ["San Diego", "Los Angeles", "Seattle", "San Francisco"],
+    question: "What city hosts the majority of the Top Gun flight training?"
+  },
+  {
+    answer: 0,
+    options: ["Drone warfare", "International conflict", "Retirement of Maverick", "Reunion of old pilots"],
+    question: "What is the main premise of Top Gun: Maverick?"
+  },
+  {
+    answer: 3,
+    options: ["An old rival", "A new recruit", "A government official", "Unknown"],
+    question: "Who is the main antagonist in Top Gun: Maverick?"
+  }
+];
