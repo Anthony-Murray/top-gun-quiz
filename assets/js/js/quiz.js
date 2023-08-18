abe6ced47d18210d00154de012ffd4e9264943b0
Anthony Murray <anthonymurray1@gmail.com>
2023-08-18 20:02:54 +0100

    Test: show questions when start button is clicked

--- assets/js/js/quiz.js
+++ assets/js/js/quiz.js
@@ -1,6 +1,8 @@
 const startButton = document.getElementById('start-btn');
+const optionsContainer = document.getElementById('options-container');
 
 startButton.addEventListener('click', () => {
   console.log('Test start button.');
   startButton.style.display = 'none';
+  optionsContainer.style.display = 'block';
 });
